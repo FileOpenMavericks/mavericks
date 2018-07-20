4542dac25e540f6e3884df5f944d5cd08e19a822
nkalscheuer <nkalscheuer@gmail.com>
2018-07-19 18:53:54 -0700

    Adding API retrieval in open geo component

--- src/components/Dashboard/Analytics/Document/OpenGeo.vue
+++ src/components/Dashboard/Analytics/Document/OpenGeo.vue
@@ -54,39 +54,67 @@ export default {
         }
     },
     mounted: function() {
-        var margin = this.getMargin();
-        var height = window.innerHeight - margin.top - margin.bottom;
-        var width = window.innerWidth - margin.left - margin.right;
-        var colors = d3.scaleOrdinal(d3.schemeCategory10);
-        
-        //Acquiring the projection via the margin
-        var projection = this.getProjection(width, height);
-        console.log("Generated projection");
-        console.log(projection);
-        
-        //Setting a path with the projection
-        var path = d3.geoPath()
-                    .projection(projection);
-        console.log("PATH:");
-        console.log(path());
-        var svg = this.makeSvg(path, width, height);
-        console.log("SVG:");
-        console.log(svg);
-        var g = this.makeG(svg, margin);
-        console.log("G unit:");
-        console.log(g);
-
-        //Get tool tip
-        var tooltip = this.makeToolTip();
+        //Doc Ref Id: 9c80af62b1094bdfab633019b2d10c1e
+        this.updateMap('9c80af62b1094bdfab633019b2d10c1e');
         
         
     },
     created: function() {},
     methods: {
+        updateMap(docRefId){
+            let $this = this;
+            $this.$http.get('https://pubtest.fileopen.com/api/analytics/file/' + docRefId).then(response => {
+                $this.fileData = response.body;
+                // NOTE: Data is an array of entries, this prints the first entry
+                console.log($this.fileData);
+
+                // NOTE: This is where I would call it calculate the data and create the graphic
+                //       However, it currently uses static test data so it isn't necessary
+                this.renderData($this.fileData);
+            }, response => {
+                console.error(response);
+            });
+        },
+        renderData(data){
+            var margin = this.getMargin();
+            var height = window.innerHeight - margin.top - margin.bottom;
+            var width = window.innerWidth - margin.left - margin.right;
+            var colors = d3.scaleOrdinal(d3.schemeCategory10);
+            
+            //Acquiring the projection via the margin
+            var projection = this.getProjection(width, height);
+            console.log("Generated projection");
+            console.log(projection);
+            
+            //Setting a path with the projection
+            var path = d3.geoPath()
+                        .projection(projection);
+            console.log("PATH:");
+            console.log(path());
+            var svg = this.makeSvg(path, width, height);
+            console.log("SVG:");
+            console.log(svg);
+            var g = this.makeG(svg, margin);
+            console.log("G unit:");
+            console.log(g);
+
+            //Get tool tip
+            var tooltip = this.makeToolTip();
+
+            //Queue
+            var files = ["https://raw.githubusercontent.com/sharad97/CMPS-165/master/Project/world_110m.json"];
+
+            //Main function
+            var promises = [];
+
+            files.forEach(function(url) {
+                promises.push(d3.json(url))
+            });
+        },
         getMargin(){
-            var margin = {top: 0, right: 0, bottom: 0, left: 0},
-            width = window.innerWidth - margin.left - margin.right,
-            height  = window.innerHeight - margin.top - margin.bottom;
+            var margin = {top: 0, right: 0, bottom: 0, left: 0};
+            var width = window.innerWidth - margin.left - margin.right;
+            var height  = window.innerHeight - margin.top - margin.bottom;
             return margin;
         },
         getProjection(width, height){
