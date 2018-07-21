df12a30bdb3aad2abd02f634b040a6c552316b19
Randall Li <rhli@ucsc.edu>
2018-07-21 16:44:17 -0700

    Basic template of Heat Chart Up

--- src/components/Dashboard/Analytics/Document/OpenChart.vue
+++ src/components/Dashboard/Analytics/Document/OpenChart.vue
@@ -6,7 +6,7 @@
                 <v-text-field prepend-icon="search" v-model="searchTerm" hide-details single-line></v-text-field>
             </v-toolbar>
             <v-card-text>
-                <div id="heatchart-container" class="svg-container"></div>
+                <div id="heatChart-container" class="svg-container"></div>
             </v-card-text>
         </v-card>
     </v-layout>
@@ -45,12 +45,49 @@ export default {
               console.log($this.linkData);
               var output = foPp.countData(this.linkData, "user.email");
               console.log(output);
-
+              $this.renderData($this.linkData);
               // NOTE: This is where I would call it calculate the data and create the graphic
               //       However, it currently uses static test data so it isn't necessary
           }, response => {
               console.error(response);
           });
+      },
+      renderData(sessionData){
+        var margin = { top: 50, right: 0, bottom: 100, left: 30 },
+            width = 960 - margin.left - margin.right,
+            height = 430 - margin.top - margin.bottom,
+            gridSize = Math.floor(width / 24),
+            legendElementWidth = gridSize*2,
+            buckets = 9,
+            colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
+            days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+            times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
+            console.log("QDDWD@QF@F@F@EF@");
+        var svg = d3.select("#heatChart-container").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var dayLabels = svg.selectAll(".dayLabel")
+            .data(days)
+            .enter().append("text")
+              .text(function (d) { return d; })
+              .attr("x", 0)
+              .attr("y", function (d, i) { return i * gridSize; })
+              .style("text-anchor", "end")
+              .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
+              .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
+
+        var timeLabels = svg.selectAll(".timeLabel")
+            .data(times)
+            .enter().append("text")
+              .text(function(d) { return d; })
+              .attr("x", function(d, i) { return i * gridSize; })
+              .attr("y", 0)
+              .style("text-anchor", "middle")
+              .attr("transform", "translate(" + gridSize / 2 + ", -6)")
+              .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
       }
     }
 }
