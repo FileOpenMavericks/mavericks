93ea34890d73d4063b93d92a1b0c914df752a5ba
Randall Li <rhli@ucsc.edu>
2018-07-20 22:01:01 -0700

    PreProcessor Test Passed

--- src/components/Dashboard/Analytics/Document/OpenChart.vue
+++ src/components/Dashboard/Analytics/Document/OpenChart.vue
@@ -19,20 +19,39 @@
 
 <script>
 import * as d3 from 'd3';
-import * as topojson from 'topojson';
+import * as foPp from '../../../../FoNodeModules/FoPreprocessor';
 
 export default {
     name: 'open-chart',
-    data(){
-      return {
-      };
+    data() {
+        return {
+            searchTerm: '',
+            data: [50, 90, 20, 100, 40, 50],
+            linkData: null,
+            line: ''
+        }
     },
     mounted: function(){
-      console.log(' Fuck You')
+      this.getData();
     },
     created: function() {},
     methods: {
+      getData() {
+          let $this = this;
+          let linkId = '426732708eee4929bd0ecbe9a4fc0b18'
+          $this.$http.get('https://pubtest.fileopen.com/api/analytics/link/' + linkId).then(response => {
+              $this.linkData = response.body;
+              // NOTE: Data is an array of entries, this prints the first entry
+              console.log($this.linkData);
+              var output = foPp.countData(this.linkData, "user.email");
+              console.log(output);
 
+              // NOTE: This is where I would call it calculate the data and create the graphic
+              //       However, it currently uses static test data so it isn't necessary
+          }, response => {
+              console.error(response);
+          });
+      }
     }
 }
 </script>
